a748e5a725bf1ec7a5bbe1cb8024fafaddd997ef
Сологуб Никита Олегович <NSologub@alfabank.ru>
2022-08-23 15:55:57 +0300

    feature: 0005 - добавлен сервер minio и функция добавления стоков автомобилей в него

---
+++ src/main/java/ru/alfaleasing/dealer/offer/web/portal/service/MinIOService.java
@@ -0,0 +1,109 @@
+package ru.alfaleasing.dealer.offer.web.portal.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.ObjectWriteResponse;
+import io.minio.PutObjectArgs;
+import io.minio.UploadObjectArgs;
+import io.minio.errors.MinioException;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.alfaleasing.dealer.offer.web.portal.dto.ExcelSortedCarsResponse;
+import ru.alfaleasing.dealer.offer.web.portal.dto.XmlSortedCarsResponse;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MinIOService {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final MinioClient minioClient;
+
+    @Value("${minio.bucket}")
+    private String bucketName;
+
+    /**
+     * Для создания Json файла и отправки его в MinIO
+     *
+     * @param response данными о автомобилях в формате которые положим в MinIO в формате Json
+     */
+    @SneakyThrows
+    public void writeRequestToJsonFile(XmlSortedCarsResponse response) {
+        LocalDateTime time = LocalDateTime.now();
+        String filename = "somefiles//car" + time.toLocalDate().toString() +
+            "-" + time.toLocalTime().getHour() + "-" + time.toLocalTime().getMinute() +
+            "-" + time.toLocalTime().getSecond() + "-" + time.toLocalTime().getNano() + ".json";
+
+        try {
+            objectMapper.writeValue(new File(filename), response);
+        } catch (IOException e) {
+            System.out.println("**file does not exists");
+            log.debug("**file does not exists");
+        }
+        writeFileToMinIO(filename);
+        System.out.println("Должно быть файл записан в MinIO");
+    }
+
+    /**
+     * Для создания Json файла и отправки его в MinIO
+     *
+     * @param response данными о автомобилях в формате которые положим в MinIO в формате Json
+     */
+    @SneakyThrows
+    public void writeRequestToJsonFile(ExcelSortedCarsResponse response) {
+        LocalDateTime time = LocalDateTime.now();
+        String filename = "somefiles//car" + time.toLocalDate().toString() +
+            "-" + time.toLocalTime().getHour() + "-" + time.toLocalTime().getMinute() +
+            "-" + time.toLocalTime().getSecond() + "-" + time.toLocalTime().getNano() + ".json";
+
+        try {
+            objectMapper.writeValue(new File(filename), response);
+        } catch (IOException e) {
+            System.out.println("**file does not exists");
+            log.debug("**file does not exists");
+        }
+        writeFileToMinIO(filename);
+        System.out.println("Должно быть файл записан в MinIO");
+    }
+
+    @SneakyThrows
+    public void writeFileToMinIO(String fileName) {
+        try {
+            createBucketIfNotExists();
+
+            UploadObjectArgs args = UploadObjectArgs.builder()
+                .bucket(bucketName)
+                .object(fileName.substring("somefiles".length()))
+                .filename(fileName).build();
+            ObjectWriteResponse objectWriteResponse = minioClient.uploadObject(args);
+
+            System.out.println("objectWriteResponse = " + objectWriteResponse);
+            log.debug("objectWriteResponse = {}", objectWriteResponse);
+            System.out.println(fileName + " successfully uploaded to: container: " + bucketName + "   blob: " + fileName);
+            log.debug("{} successfully uploaded to: container: {}   blob: {}", fileName, bucketName, fileName);
+        } catch (MinioException e) {
+            System.out.println("Error occurred: " + e);
+            log.debug("Error occurred: " + e);
+        }
+    }
+
+    @SneakyThrows
+    private void createBucketIfNotExists() {
+        boolean bucketExists;
+        bucketExists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+        if (!bucketExists) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+        }
+    }
+}
