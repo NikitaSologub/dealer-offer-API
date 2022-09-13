339fd24078171778451c9c1d13dd6f9dd53b9aca
Сологуб Никита Олегович <nosologub@alfaleasing.ru>
2022-09-13 11:24:18 +0300

    feature: 0019 - добавил метод для завязки на авторизацию

--- src/main/java/ru/alfaleasing/dealer/offer/api/Application.java
+++ src/main/java/ru/alfaleasing/dealer/offer/api/Application.java
@@ -2,14 +2,12 @@ package ru.alfaleasing.dealer.offer.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import ru.alfaleasing.dealer.offer.api.queue.QueueReceiver;
 import ru.alfaleasing.dealer.offer.api.queue.QueueSender;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableBinding({QueueSender.class, QueueReceiver.class})
 @EnableSwagger2
 public class Application {
